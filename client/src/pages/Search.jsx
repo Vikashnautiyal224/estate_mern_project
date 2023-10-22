b496eaf49baab758777a05b142ae79d2226265ef
Vikashnautiyal224 <vikashnautiyal224@gmail.com>
2023-10-22 19:44:26 +0530

    solve issue in listingitem.jsx in search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ListingItem from "../components/LIstingItem";
+import ListingItem from "../components/ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
